286de33ea71f371a07212af7e72d2040ac3e90ec
weaming <garden.yuen@gmail.com>
2018-06-22 21:52:07 +0800

    ready

---
+++ lib/sogou_translate.ex
@@ -0,0 +1,7 @@
+defmodule SogouTranslate do
+  defmodule CLI do
+    def main(_args) do
+      IO.puts("hello")
+    end
+  end
+end
---
+++ test/sogou_translate_test.exs
@@ -0,0 +1,8 @@
+defmodule SogouTranslateTest do
+  use ExUnit.Case
+  doctest SogouTranslate
+
+  test "greets the world" do
+    assert SogouTranslate.hello() == :world
+  end
+end
